074d959d807d3bc7c0d73201810b0362041caa2d
jeresig <jeresig@gmail.com>
2011-02-02 06:21:04 -0800

    Made the logic for working through sites a bit more generic.

--- extract.pl
+++ extract.pl
@@ -12,51 +12,61 @@ chdir( $clean_term );
 my $units = '(?:tablespoons?|tbsps?|tsp|teaspoons?|quart|cups?|pinch|pinches|pounds?|ounces?|oz|cans?|gallons?|bottles?|packages?|envelopes?|inch|inches|whole|cloves?|heads?|slices?|thin slices?|cubes?|heads?|pieces?|bunch|bunches|dash|dashes|sprigs?|leaf|leaves|head|box|recipe)';
 my $find_ingredient = "(^[0-9/ ]+(?:\\([^\\)]+\\) )?$units?(?: piece)?|to taste|as needed|for \\w+|if needed)\\b";
 
-my $find_technique = '\b((?:(?:finely|coarsely|fully|thinly) )?(?:chopped|diced|sliced|peeled|deveined|reconstituted|beaten|drained|cubed|cooked|minced|halved|divided|undrained|cut|shredded|crushed|julienned|pitted|boiling|boiled|rinsed|cored|grated|melted|slivered|diagonally|cut|soaked|mixed|mashed|dissolved|torn|picked|trimmed|shelled|salted|packed|softened|seeded|cleaned|scaled|\w+ removed|finely ground|ground)(?: (?:thin|thinly|lengthwise|diagonally|into .*|until.*|in.*|with.*|from.*))*)\b';
+my $find_technique = '\b((?:(?:finely|coarsely|fully|thinly) )?(?:chopped|diced|sliced|peeled|deveined|reconstituted|beaten|drained|cubed|cooked|minced|halved|divided|undrained|cut|shredded|crushed|julienned|pitted|boiling|boiled|rinsed|cored|grated|melted|slivered|diagonally|cut|soaked|mixed|mashed|dissolved|torn|picked|trimmed|shelled|salted|packed|softened|seeded|cleaned|scaled|\w+ removed|finely ground|ground)(?: (?:thin|thinly|lengthwise|diagonally|(?:into|until|in|with|from) .*?(?=and|or|$)))*)\b';
 
 my $find_size = '(small|large|medium)';
 
+my %extract = (
+	"allrecipes.com" => {
+		files => 'allrecipes.com/Recipe/*/Detail.aspx',
+		title => '<title>\s*(.*?) Recipe',
+		ingredients => '(<div class="ingredients".*?</div>)',
+		ingredient => '<li.*?>(.*?)</li>'
+	}
+);
+
 my $recipes = {};
 
-if ( -d "allrecipes.com" ) {
-	$recipes->{ "allrecipes.com" } = [];
+foreach my $site ( sort keys %extract ) {
+	if ( -d $site ) {
+		$recipes->{ $site } = [];
 
-	foreach my $name ( <allrecipes.com/Recipe/*/Detail.aspx> ) {
-		my $file = `cat $name`;
-		my $recipe = {
-			name => "",
-			ingredients => {},
-			#nutrition => {}
-		};
+		my $opt = $extract{ $site };
 
-		if ( $file =~ /<title>\s*(.*?) Recipe/mi ) {
-			$recipe->{ name } = $1;
-		}
+		foreach my $name ( glob( $opt->{files} ) ) {
+			my $file = `cat $name`;
+			my $recipe = {
+				name => "",
+				ingredients => {}
+			};
 
-		# Find the ingredients
-		if ( $file =~ m!(<div class="ingredients".*?</div>)!s ) {
-			my $ingredients = $1;
+			# Extract the recipe title
+			if ( $file =~ /$opt->{title}/mi ) {
+				$recipe->{ name } = $1;
+			}
 
-			while ( $ingredients =~ m!<li.*?>(.*?)</li>!gs ) {
-				my ( $name, $data ) = get_ingredient( $1 );
+			# Find the ingredients
+			if ( $file =~ m!$opt->{ingredients}!s ) {
+				my $ingredients = $1;
 
-				if ( $name ) {
-					$recipe->{ ingredients }{ $name } = $data;
+				# Get the individual ingredients
+				while ( $ingredients =~ m!$opt->{ingredient}!gs ) {
+					my ( $name, $data ) = get_ingredient( $1 );
+
+					if ( $name ) {
+						$recipe->{ ingredients }{ $name } = $data;
+					}
 				}
 			}
-		}
 
-		push( @{ $recipes->{ "allrecipes.com" } }, $recipe );
+			push( @{ $recipes->{ $site } }, $recipe );
+		}
 	}
 }
 
 $Data::Dumper::Indent = 1;
 
 print Dumper( $recipes );
-
-if ( -d "www.food.com" ) {
-
-}
 
 sub get_ingredient {
 	my $ingredient = clean( lc($_[0]) );
